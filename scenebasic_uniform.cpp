d37d8835d4583cc43ea9ec0a43ec14f3e45d328c
Kuramura300 <chloeburgess21@yahoo.co.uk>
2023-03-23 18:11:10 +0000

    added textures and adjusted lighting, added comments

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -18,12 +18,14 @@ using std::endl;
 
 #include "helper/glutils.h"
 #include "helper/torus.h"
+#include "helper/texture.h"
 
 using glm::vec3;
 using glm::mat4;
 
 SceneBasic_Uniform::SceneBasic_Uniform() : plane(10.0f, 10.0f, 100, 100)
 {
+    // Load models from file
     meshes.push_back(ObjMesh::load("../Project_Template/media/charactermodel.obj", true));
     meshes.push_back(ObjMesh::load("../Project_Template/media/charactermodel2.obj", true));
 }
@@ -34,9 +36,11 @@ void SceneBasic_Uniform::initScene()
 
     glEnable(GL_DEPTH_TEST);
 
+    // Setup camera view
     view = glm::lookAt(vec3(0.5f, 0.75f, 0.75f), vec3(0.0f, 0.25f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
     projection = mat4(1.0f);
 
+    // Setup lights
     float x, z;
     for (int i = 0; i < 3; i++)
     {
@@ -48,14 +52,16 @@ void SceneBasic_Uniform::initScene()
             1.0f, 1.0f));
     }
 
+    // Set diffuse and specular light intensity uniforms
     prog.setUniform("lights[0].L", vec3(0.0f, 0.0f, 0.8f));
     prog.setUniform("lights[1].L", vec3(0.0f, 0.8f, 0.0f));
     prog.setUniform("lights[2].L", vec3(0.8f, 0.0f, 0.0f));
 
-    // set ambient light uniforms (what values should go in vec3?)
+    // Set ambient light uniforms
     prog.setUniform("lights[0].La", vec3(0.2f, 0.2f, 0.2f));
-    prog.setUniform("lights[1].La", vec3(0.2f, 0.2f, 0.2f));
-    prog.setUniform("lights[2].La", vec3(0.2f, 0.2f, 0.2f));
+    prog.setUniform("lights[1].La", vec3(0.1f, 0.1f, 0.1f));
+    prog.setUniform("lights[2].La", vec3(0.3f, 0.3f, 0.3f));
+
 }
 
 void SceneBasic_Uniform::compile()
@@ -81,9 +87,14 @@ void SceneBasic_Uniform::render()
     glClear(GL_COLOR_BUFFER_BIT);
     glClear(GL_DEPTH_BUFFER_BIT);
 
-    prog.setUniform("Material.Kd", 0.4f, 0.4f, 0.4f);
+    // Draw first character
+    GLuint texID = Texture::loadTexture("../Project_Template/media/texture/BlueGradient.png");
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texID);
+
+    prog.setUniform("Material.Kd", 0.1f, 0.1f, 0.1f);
     prog.setUniform("Material.Ks", 0.9f, 0.9f, 0.9f);
-    prog.setUniform("Material.Ka", 0.5f, 0.5f, 0.5f);
+    prog.setUniform("Material.Ka", 0.1f, 0.1f, 0.1f);
     prog.setUniform("Material.Shininess", 180.0f);
 
     model = mat4(1.0f);
@@ -93,13 +104,20 @@ void SceneBasic_Uniform::render()
     setMatrices();
     meshes[0]->render();
 
+    // Draw second character
     model = mat4(1.0f);
     model = glm::translate(model, vec3(-0.45f, 0, -0.5f));
     model = glm::rotate(model, glm::radians(-90.0f), vec3(0.0f, 1.0f, 0.0f));
     model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
     setMatrices();
+
+    texID = Texture::loadTexture("../Project_Template/media/texture/BlackGradient.png");
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texID);
+
     meshes[1]->render();
 
+    // Draw plane
     prog.setUniform("Material.Kd", 0.1f, 0.1f, 0.1f);
     prog.setUniform("Material.Ks", 0.9f, 0.9f, 0.9f);
     prog.setUniform("Material.Ka", 0.1f, 0.1f, 0.1f);
@@ -108,6 +126,11 @@ void SceneBasic_Uniform::render()
     model = mat4(1.0f);
     model = glm::translate(model, vec3(0.0f, -0.45f, 0.0f));
     setMatrices();
+
+    texID = Texture::loadTexture("../Project_Template/media/texture/meadow.jpg");
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texID);
+
     plane.render();
 }
 
